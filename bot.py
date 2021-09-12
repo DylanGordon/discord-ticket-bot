127165e79d9a4193786c0278bd18b6e4b93aa99e
DylanGordon <dylanjgordon57@gmail.com>
2021-09-11 22:17:16 -0400

    first commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+import os
+from discord_components import ComponentsBot
+from dotenv import load_dotenv
+
+# Loads Environment Variables
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.members = True
+bot = ComponentsBot(os.getenv("prefix"), intents=intents, activity=discord.Activity(type=discord.ActivityType.watching, name="For New Tickets"), status=discord.Status.online)
+bot.remove_command('help')
+
+# Event Starts When Bot Is Online
+@bot.event
+async def on_ready():
+    print('Ticket Bot Online!')
+
+# Loads ALl Py Files On Startup.
+for file in os.listdir('./cogs'):
+    if file.endswith('.py'):
+        bot.load_extension(f'cogs.{file[:-3]}')
+
+bot.run(os.getenv('token'))
